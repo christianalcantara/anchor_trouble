1e22a5f6200008a7a03470c726dce814a20b9b4b
christian <christian.douglas.alcantara@gmail.com>
2016-09-30 23:49:11 -0300

    debug mode

--- anchor_trouble/settings.py
+++ anchor_trouble/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j*s@rh!)sc57id7gzhslxk#yoj&u#2y-#um$2m_f#j$pek&o2z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
